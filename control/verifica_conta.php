5957fb1aab8503346f34f3e00d40a85243986a77
Marco Aurélio <Marco Aurlio@MARCO-NOTE>
2014-05-24 18:07:20 -0300

    Tela de Login implementada e gerado o script do banco de dados.

--- control/verifica_conta.php
+++ control/verifica_conta.php
@@ -1,10 +1,23 @@
 <?php
+
 require_once '../control/conexao.class.php';
 
-if(!isset($_POST["login_email"]) or !isset($_POST["login_senha"]) or $_POST["login_senha"]=="" or $_POST["login_email"]==""){
+$login = $_POST["login_email"];
+$senha = $_POST["login_senha"];
+
+if (!isset($_POST["login_email"]) or ! isset($_POST["login_senha"]) or $_POST["login_senha"] == "" or $_POST["login_email"] == "") {
     header("Location: http://localhost/site-php-funshop/view/login.php?action=loginerror");
-}else{
-    session_start();
-    $_SESSION["ds_email"] = $_POST["login_email"];
-    header("Location: ../view/index.php");
+} else {
+    $con = mysql_connect("localhost", "root", "root") or die(mysql_error());
+    $sel = mysql_select_db("bd_funshop");
+    $result = @mysql_query("SELECT u.nm_usuario, u.vl_saldo FROM tb_usuario u, tb_login l "
+            . "WHERE l.ds_login='$login' AND l.ds_senha='$senha' AND l.cd_usuario=u.cd_usuario") or die(mysql_error());
+    
+    if (mysql_num_rows($result) > 0) {
+        session_start();
+        $valor = mysql_fetch_array($result);
+        $_SESSION["ds_email"] = $_POST["login_email"];
+        $_SESSION["nm_usuario"] = $valor["nm_usuario"];
+        header("Location: ../view/index.php");
+    }
 }
