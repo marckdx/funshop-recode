06d3e12c32daeac54a05dddce77cca2ad5fc8587
Valmir Santos da Silva <valmirssl@gmail.com>
2014-05-27 20:52:52 -0300

    Script SQL modificado para o status atual, com os campos dos produtos arrumados

--- content/bd_funshop.sql
+++ content/bd_funshop.sql
@@ -28,7 +28,7 @@ CREATE TABLE `tb_login` (
   `cd_login` int(5) NOT NULL AUTO_INCREMENT,
   `cd_usuario` int(5) NOT NULL,
   `ds_login` varchar(32) NOT NULL,
-  `ds_senha` varchar(30) NOT NULL,
+  `ds_senha` varchar(32) NOT NULL,
   PRIMARY KEY (`cd_login`),
   KEY `cd_usuario` (`cd_usuario`),
   CONSTRAINT `tb_login_ibfk_1` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`cd_usuario`)
@@ -59,7 +59,7 @@ CREATE TABLE `tb_produto` (
   `qt_produto` int(11) NOT NULL,
   `vl_produto` double(5,2) NOT NULL,
   `pc_desconto_produto` double(3,2) DEFAULT NULL,
-  `st_produto` tinyint(1) NOT NULL,
+  `st_produto` varchar(1) DEFAULT NULL,
   `ds_garantia_produto` varchar(200) NOT NULL,
   `ds_link_produto` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`cd_produto`)
@@ -72,7 +72,7 @@ CREATE TABLE `tb_produto` (
 
 LOCK TABLES `tb_produto` WRITE;
 /*!40000 ALTER TABLE `tb_produto` DISABLE KEYS */;
-INSERT INTO `tb_produto` VALUES (1,'PS3 Ratchet & Clank Future: Tools of Destruction','Desde 2002, Ratchet vem promovendo a destruição por onde passa.Ele tem uma aparência de mascote, mas é mestre no uso de armas pesadas.Com essa combinação, se tornou uma das séries mais populares dos consoles da Sony.',50,79.00,0.05,1,'90 dias - Contra defeitos de Fabricação.Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/1.jpg'),(2,'PS3 Dead To Rights - Retribution','Afastado dos videogames desde 2005, o policial linha dura Jack Slate volta da aposentadoria e traz, claro, seu velho parceiro canino Shadow de volta. É o reinício da franquia \"Dead to Rights\", que começou como uma mera imitação de \"Max Payne\" e acabou rendendo continuações e até mesmo aventuras paralelas para diversas plataformas.',40,99.00,0.05,1,'90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/2.jpg'),(3,'PS3 FaceBreaker','Lá se vão duas décadas desde Mike Tyson s Punch Out e poucos jogos de boxe depois dele tentaram unir o esporte a elementos cômicos, e menos ainda obtiveram algum sucesso. Com o desejo da EA Sports em inaugurar uma nova linha de títulos casuais, FaceBreaker evoca o clássico da década de 80 em bem-humorados duelos nos ringues.',20,49.00,0.05,1,'90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/3.jpg'),(4,'DEU A LOUCA NA FUN SHOP PS3 PES2014 - Pro Evolution Soccer','Pro Evolution Soccer 2014 marcará um novo começo para a popular série, com uma nova engine trazendo maior avanço para o jogo desde a sua criação. A new-engine usa o renomado FOX-Engine da Kojima Productions em seu núcleo, ampliado e melhorado para atender as demandas complexas e sob medida de um jogo de futebol.',30,114.80,0.05,0,'90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/4.jpg');
+INSERT INTO `tb_produto` VALUES (1,'PS3 Ratchet & Clank Future: Tools of Destruction','Desde 2002, Ratchet vem promovendo a destruição por onde passa.Ele tem uma aparência de mascote, mas é mestre no uso de armas pesadas.Com essa combinação, se tornou uma das séries mais populares dos consoles da Sony.',50,79.00,0.95,'1','90 dias - Contra defeitos de Fabricação.Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/1.jpg'),(2,'PS3 Dead To Rights - Retribution','Afastado dos videogames desde 2005, o policial linha dura Jack Slate volta da aposentadoria e traz, claro, seu velho parceiro canino Shadow de volta. É o reinício da franquia \"Dead to Rights\", que começou como uma mera imitação de \"Max Payne\" e acabou rendendo continuações e até mesmo aventuras paralelas para diversas plataformas.',40,99.00,0.95,'1','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/2.jpg'),(3,'PS3 FaceBreaker','Lá se vão duas décadas desde Mike Tyson s Punch Out e poucos jogos de boxe depois dele tentaram unir o esporte a elementos cômicos, e menos ainda obtiveram algum sucesso. Com o desejo da EA Sports em inaugurar uma nova linha de títulos casuais, FaceBreaker evoca o clássico da década de 80 em bem-humorados duelos nos ringues.',20,49.00,0.95,'1','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/3.jpg'),(4,'DEU A LOUCA NA FUN SHOP PS3 PES2014 - Pro Evolution Soccer','Pro Evolution Soccer 2014 marcará um novo começo para a popular série, com uma nova engine trazendo maior avanço para o jogo desde a sua criação. A new-engine usa o renomado FOX-Engine da Kojima Productions em seu núcleo, ampliado e melhorado para atender as demandas complexas e sob medida de um jogo de futebol.',30,114.80,0.95,'0','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/4.jpg');
 /*!40000 ALTER TABLE `tb_produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-27 10:22:18
+-- Dump completed on 2014-05-27 20:49:18
