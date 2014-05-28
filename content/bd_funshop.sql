ebe39d1f6d70ae74d2d982e765c5cb2a6ccdfa20
Marco Aurélio <Marco Aurlio@MARCO-NOTE>
2014-05-28 14:35:52 -0300

    Finalização[2] do arquivo bd_funshop.sql.

--- content/bd_funshop.sql
+++ content/bd_funshop.sql
@@ -18,6 +18,31 @@ USE `bd_funshop`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario` (
+  `cd_usuario` int(5) NOT NULL AUTO_INCREMENT,
+  `nm_usuario` varchar(50) NOT NULL,
+  `vl_saldo` decimal(4,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`cd_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+INSERT INTO `tb_usuario` VALUES (1,'Marco Aurélio Lima',99.99),(2,'Jhonathan Rodrigues',99.99),(3,'Augusto Abranches',99.99),(4,'Gilberto Donizeti Fioravante',23.00),(5,'Valmir Santos da Silva',30.00),(6,'Vagner dos Santos Macedo',3.00);
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
 -- Table structure for table `tb_login`
 --
 
@@ -75,32 +100,6 @@ LOCK TABLES `tb_produto` WRITE;
 INSERT INTO `tb_produto` VALUES (1,'PS3 Ratchet & Clank Future: Tools of Destruction','Desde 2002, Ratchet vem promovendo a destruição por onde passa.Ele tem uma aparência de mascote, mas é mestre no uso de armas pesadas.Com essa combinação, se tornou uma das séries mais populares dos consoles da Sony.',50,79.00,0.05,'1','90 dias - Contra defeitos de Fabricação.Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/1.jpg'),(2,'PS3 Dead To Rights - Retribution','Afastado dos videogames desde 2005, o policial linha dura Jack Slate volta da aposentadoria e traz, claro, seu velho parceiro canino Shadow de volta. É o reinício da franquia \"Dead to Rights\", que começou como uma mera imitação de \"Max Payne\" e acabou rendendo continuações e até mesmo aventuras paralelas para diversas plataformas.',40,99.00,0.05,'1','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/2.jpg'),(3,'PS3 FaceBreaker','Lá se vão duas décadas desde Mike Tyson s Punch Out e poucos jogos de boxe depois dele tentaram unir o esporte a elementos cômicos, e menos ainda obtiveram algum sucesso. Com o desejo da EA Sports em inaugurar uma nova linha de títulos casuais, FaceBreaker evoca o clássico da década de 80 em bem-humorados duelos nos ringues.',20,49.00,0.05,'1','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/3.jpg'),(4,'DEU A LOUCA NA FUN SHOP PS3 PES2014 - Pro Evolution Soccer','Pro Evolution Soccer 2014 marcará um novo começo para a popular série, com uma nova engine trazendo maior avanço para o jogo desde a sua criação. A new-engine usa o renomado FOX-Engine da Kojima Productions em seu núcleo, ampliado e melhorado para atender as demandas complexas e sob medida de um jogo de futebol.',30,114.80,0.05,'0','90 dias - Contra defeitos de Fabricação. Não Trocamos CDs, DVDs ou Blu-ray riscados.','../img_bd/4.jpg');
 /*!40000 ALTER TABLE `tb_produto` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `tb_usuario`
---
-
-DROP TABLE IF EXISTS `tb_usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tb_usuario` (
-  `cd_usuario` int(5) NOT NULL AUTO_INCREMENT,
-  `nm_usuario` varchar(50) NOT NULL,
-  `vl_saldo` decimal(4,2) NOT NULL DEFAULT '0.00',
-  PRIMARY KEY (`cd_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tb_usuario`
---
-
-LOCK TABLES `tb_usuario` WRITE;
-/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
-INSERT INTO `tb_usuario` VALUES (1,'Marco Aurélio Lima',99.99),(2,'Jhonathan Rodrigues',99.99),(3,'Augusto Abranches',99.99),(4,'Gilberto Donizeti Fioravante',23.00),(5,'Valmir Santos da Silva',30.00),(6,'Vagner dos Santos Macedo',3.00);
-/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
